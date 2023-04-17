09fd5143981495111dc4e4f6e179633bbb508432
Emmanuel Tamburini <emmanueltamburini@gmail.com>
2023-04-16 21:13:33 -0400

    IOS configuration for google map has added

--- ios/App/AppDelegate.mm
+++ ios/App/AppDelegate.mm
@@ -6,6 +6,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  [GMSServices provideAPIKey:@"_YOUR_API_KEY_"];
   self.moduleName = @"App";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
